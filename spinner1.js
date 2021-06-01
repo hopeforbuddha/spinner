2428943f3b37a67b64b171d55472c95b7b05d562
Jacob <jacob.fitzgerald@outlook.com>
2021-06-01 19:07:43 +0000

    created simple spinner

---
+++ spinner1.js
@@ -0,0 +1,43 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r|   '); 
+}, 1000);
+
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1600);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1900);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r|   '); 
+}, 2100);
+// ... fill in the rest yourself ...
